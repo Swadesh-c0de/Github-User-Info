8d47ae507f0f9ed545aba5955ac2e463c7cdabdb
Swadesh-c0de <patelswadesh7@gmail.com>
2024-11-01 01:17:07 +0530

    adding some changes

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ async function fetchData() {
 
 function setData(data) {
     const outputBox = document.createElement('div');
-    outputBox.setAttribute('id','output-div');
+    outputBox.setAttribute('id', 'output-div');
     home.append(outputBox);
 
     // Profile Image
@@ -88,4 +88,16 @@ function setData(data) {
     <path d="m12.596 11.596-3.535 3.536a1.5 1.5 0 0 1-2.122 0l-3.535-3.536a6.5 6.5 0 1 1 9.192-9.193 6.5 6.5 0 0 1 0 9.193Zm-1.06-8.132v-.001a5 5 0 1 0-7.072 7.072L8 14.07l3.536-3.534a5 5 0 0 0 0-7.072ZM8 9a2 2 0 1 1-.001-3.999A2 2 0 0 1 8 9Z">
     </path></svg> ${data.location}`;
     outputBox.append(address);
+
+    // Public Repo
+    const fetchRepo = async () => {
+        const response = await fetch('data.repos_url', {
+            method: 'GET',
+        })
+            .then(resonse => resonse.json())
+            .then(data =>  () => {
+                
+            })
+            .catch(error => console.error("Error occers while fetching user's public repo: \n", error));
+    }
 }
