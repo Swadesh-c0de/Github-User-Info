32782ce73837f61548f012d4f728e2cc5e3953cf
Swadesh-c0de <patelswadesh7@gmail.com>
2024-11-01 02:57:07 +0530

    adding repo fetch

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ usernameInput.addEventListener('keypress', (element) => searchData(element));
 function searchData(e) {
     if (e.key === 'Enter') {
         if (usernameInput.value !== "") {
-
             fetchData();
         }
     }
@@ -64,13 +63,22 @@ function setData(data) {
     // Follow
     const follow = document.createElement('p');
     follow.setAttribute('class', 'follow');
-    follow.innerHTML = `<span>${data.followers}</span> Followers • <span>${data.following}</span> Following`;
+    follow.innerHTML = `<span id='follow'>${data.followers}</span> Followers • <span id='follow'>${data.following}</span> Following`;
 
     nameDiv.append(follow);
+
+    // Details
+    const address = document.createElement('div');
+    address.setAttribute('class', 'address');
+    address.innerHTML = `<svg class="octicon octicon-location" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true">
+    <path d="m12.596 11.596-3.535 3.536a1.5 1.5 0 0 1-2.122 0l-3.535-3.536a6.5 6.5 0 1 1 9.192-9.193 6.5 6.5 0 0 1 0 9.193Zm-1.06-8.132v-.001a5 5 0 1 0-7.072 7.072L8 14.07l3.536-3.534a5 5 0 0 0 0-7.072ZM8 9a2 2 0 1 1-.001-3.999A2 2 0 0 1 8 9Z">
+    </path></svg> ${data.location}`;
+    nameDiv.append(address);
 
     // Profile
     const profile = document.createElement('a')
     profile.setAttribute('href', `${data.html_url}`);
+    profile.setAttribute('id','profileA');
     profile.innerText = 'Visit Profile';
 
     nameDiv.append(profile);
@@ -81,23 +89,38 @@ function setData(data) {
     bio.innerText = `${data.bio}`;
     outputBox.append(bio);
 
-    // Details
-    const address = document.createElement('div');
-    address.setAttribute('class', 'address');
-    address.innerHTML = `<svg class="octicon octicon-location" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true">
-    <path d="m12.596 11.596-3.535 3.536a1.5 1.5 0 0 1-2.122 0l-3.535-3.536a6.5 6.5 0 1 1 9.192-9.193 6.5 6.5 0 0 1 0 9.193Zm-1.06-8.132v-.001a5 5 0 1 0-7.072 7.072L8 14.07l3.536-3.534a5 5 0 0 0 0-7.072ZM8 9a2 2 0 1 1-.001-3.999A2 2 0 0 1 8 9Z">
-    </path></svg> ${data.location}`;
-    outputBox.append(address);
-
     // Public Repo
-    const fetchRepo = async () => {
-        const response = await fetch('data.repos_url', {
+    fetchRepo(data);
+
+    async function fetchRepo(e){
+        const response = await fetch(`${e.repos_url}`, {
             method: 'GET',
         })
-            .then(resonse => resonse.json())
-            .then(data =>  () => {
-                
+            .then(response => response.json())
+            .then(repoData => {
+                setRepo(repoData);
+                console.log("Repo data ", repoData);
             })
             .catch(error => console.error("Error occers while fetching user's public repo: \n", error));
+    }
+
+    function setRepo(e){   
+        const ul = document.createElement('ul');
+        ul.setAttribute('class','repoUList');
+        ul.innerHTML = "<span class='spanRepo'>Repositories</span>";
+        outputBox.append(ul);
+    
+        let length = e.length;
+        let a = 0;
+        while(a<length){
+            const li = document.createElement('li');
+            const i = document.createElement('a');
+            i.setAttribute('href',`${e[a].html_url}`);
+            i.setAttribute('id','repoList');
+            i.innerText = `${e[a].name}`;
+            li.append(i);
+            ul.append(li);
+            a = a+1;
+        }
     }
 }
